0ea414d1475d99ae671f2555fcbfc1af0915ec7d
Chris Williams <chrismwilliams@users.noreply.github.com>
2024-03-09 10:37:44 +0000

    remove partytown due to build noise

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown(), sitemap()],
+  integrations: [sitemap()],
   vite: {
     // option applyBaseStyles doesn't seem to do much atm
     plugins: [tailwindcss({ applyBaseStyles: false })],
