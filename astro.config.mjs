9f6f4e1c8ad6eb39e7becc8758fb942902cc5216
Chris Williams <chrismwilliams@users.noreply.github.com>
2024-03-09 10:13:38 +0000

    clear house and break stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), partytown(), sitemap()]
+  integrations: [partytown(), sitemap()],
+  vite: {
+    // option applyBaseStyles doesn't seem to do much atm
+    plugins: [tailwindcss({ applyBaseStyles: false })],
+  },
 });
